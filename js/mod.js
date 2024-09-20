932c4bff62136f5dc3c2ed8b5e8553e1abd52af3
IdleGaming <83841877+HiGamezYT@users.noreply.github.com>
2024-09-20 00:18:15 -0700

    test
    test

--- js/mod.js
+++ js/mod.js
@@ -19,7 +19,7 @@ let VERSION = {
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
 		- The mod is now in beta<br>
-		- Nothing else`
+		- test`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
